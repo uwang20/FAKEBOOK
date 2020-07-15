779e733a5949357a5e6578b688c677f62a5a8b8d
Uwang20 <ohjuan205@gmail.com>
2020-07-15 23:38:23 +0800

    'save na'

--- public/js/app.js
+++ public/js/app.js
@@ -86783,8 +86783,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\john .MSI\Desktop\laravel\laravelPractice\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\john .MSI\Desktop\laravel\laravelPractice\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\john .MSI\Desktop\laravel\FAKEBOOK\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\john .MSI\Desktop\laravel\FAKEBOOK\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
